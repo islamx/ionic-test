a3c663aa999e9d8c81a3d2d6f7a3bfd9d87b9fd9
Islam Mohamed <Islam.abdelzaher@live.com>
2016-03-24 13:29:13 +0200

    first commit

---
+++ www/js/app.js
@@ -0,0 +1,66 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('ionicApp', ['ionic'])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('app', {
+      url: "/app",
+      abstract: true,
+      templateUrl: "app.html"
+    })
+    .state('app.home', {
+      url: "/home",
+      views: {
+        'appContent' :{
+          templateUrl: "home.html",
+          controller : "HomeController"
+        }
+      }
+    })
+  
+  $urlRouterProvider.otherwise("/app/home");
+})
+
+.controller('AppController', function($scope, $ionicSideMenuDelegate) {
+  $scope.toggleLeft = function() {
+    $ionicSideMenuDelegate.toggleLeft();
+  };
+})
+
+.controller("HomeController", function($scope) {
+  
+})
+
+.controller("CartController", function($scope) {
+  
+  $scope.data = {
+    items : []
+  };
+  
+  for(var i = 0; i < 25; i++) {
+    $scope.data.items.push({
+      id : i,
+      label : "Item " + i
+    })
+  }
+  
+})
+
+.directive("ionCart", function() {
+  return {
+    restrict : "E",
+    templateUrl : "ionCart.html"
+  }
+})
+
+.directive("ionPurchase", function() {
+  return {
+    restrict : "E",
+    template : "<h2>This is Ion Purchase</h2>"
+  }
+})
